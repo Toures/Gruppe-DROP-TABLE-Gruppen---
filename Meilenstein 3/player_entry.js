8e8e5b1b60de62bf88a5431143345dccc38dca49
Toures <Mike.Hukiewitz@gmx.de>
2015-11-18 14:54:59 +0100

    + Meilenstein 3 komplett!

--- Meilenstein 3/player_entry.js
+++ Meilenstein 3/player_entry.js
@@ -1,50 +1,66 @@
 var firstError = null;
-
 function onSubmit() {
-    var name  = document.getElementById ("name");
-    var vorname  = document.getElementById ("vorname");
-    var verein  = document.getElementById ("verein");
-    var coach  = document.getElementById ("coach");
-    var assistentencoach  = document.getElementById ("assistentencoach");
-    var aktiv  = document.getElementById ("aktiv");
-    var position  = document.getElementById ("position");
-    var trikotnummer  = document.getElementById ("trikotnummer");
-    var jahrgang  = document.getElementById ("jahrgang");
-    var favorit = document.getElementById ("favorit");
-
+    firstError = null;
+    var name = document.getElementById('name');
+    var vorname = document.getElementById('vorname');
+    var verein = document.getElementById('verein');
+    var coach = document.getElementById('coach');
+    var assistentencoach = document.getElementById('assistentencoach');
+    var position = document.getElementById('position');
+    var trikotnummer = document.getElementById('trikotnummer');
+    var jahrgang = document.getElementById('jahrgang');
+    var favorit = document.getElementById('favorit');
     checkName(name);
     checkName(vorname);
     checkName(verein);
     checkName(coach);
     checkName(assistentencoach);
-
-    if(trikotnummer.search(/(^[4-9]$)(^1[0-5]$)/) == -1) {
-	trikotnummer.style.borderColor = "red";
-	if(firstError == null) {
+    if (trikotnummer.value == null || parseInt(trikotnummer.value) > 15
+       || parseInt(trikotnummer.value) < 4 ) {
+	trikotnummer.style.borderColor = 'red';
+	if (firstError == null) {
 	    firstError = trikotnummer;
 	}
-    }
-
-    if(jahrgang.search(/(^1[0-9]{3}$)(^200[0-9]$)(^201[0-5]$)/) == -1) {
-	jahrgang.style.borderColor = "red";
-	if(firstError == null)
+    } else {
+	trikotnummer.style.borderColor = 'green';
+}
+    if (jahrgang.value == null || parseInt(jahrgang.value) > 2015
+       || parseInt(jahrgang.value) < 0 ) {
+	jahrgang.style.borderColor = 'red';
+	if (firstError == null)
 	    firstError = jahrgang;
-    }
-
-    if(firstError != null) {
-	alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+    } else {
+	jahrgang.style.borderColor = 'green';
+}
+    if (firstError != null) {
+	alert('Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben');
 	firstError.focus();
     }
+}
 
-    function checkName(name) {
-	if(name.search(/^[a-zA-Z]*$/) == -1) {
-	    name.style.borderColor = "red";
-	    if(firstError == null)
-		firstError = name;
-	    return false;
+function checkBoxes(box) {
+    var aktivJa = document.getElementById('aktivJa');
+    var aktivNein = document.getElementById('aktivNein');
+    if(aktivJa.checked == true && aktivNein.checked == true) {
+	if(box == aktivJa) {
+	    aktivNein.checked = false;
+	} else {
+	    aktivJa.checked = false;
 	}
-	else {
-	    name.style.borderColor = "green";
-	    return true;
-	}
+    } else if(aktivJa.checked == false && aktivNein.checked == false) {
+	box.checked = true;
+    }
+}
+
+function checkName(name) {
+    if (name.value == "" || name.value.search(/^[a-zA-Z]*$/) == - 1) {
+	name.style.borderColor = 'red';
+	if (firstError == null)
+            firstError = name;
+	return false;
+    } 
+    else {
+	name.style.borderColor = 'green';
+	return true;
+    }
 }
