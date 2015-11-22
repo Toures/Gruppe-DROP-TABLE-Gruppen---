ff7d1087a894d13a3764f8be93fe11f99e4583c7
Toures <Mike.Hukiewitz@gmx.de>
2015-11-22 16:45:18 +0100

    + Tabelle funktioniert

--- Meilenstein 4/player_detail.js
+++ Meilenstein 4/player_detail.js
@@ -1,20 +1,16 @@
-
-
-
-var tabledata = new XMLHttpRequest();
-tabledata.open("GET","/data.json");
+﻿var tabledata = new XMLHttpRequest();
+tabledata.open("GET","./data.json");
 tabledata.onreadystatechange = callbackHandler;
-tabledata.send("GET", "/data.json");
+tabledata.send();
 
 function callbackHandler(){
-	if((tabledata.readystate ==4)
-		&&(tabledata.status==200)){
+	if((tabledata.readyState==4)&&(tabledata.status==200)&&tabledata.responseText!=null){
 		tabledraw(tabledata.responseText);
 	}
 }
 
-function tabledraw(array) {
-	var arr = JSON.parse(array);
+function tabledraw(data) {
+	var arr = eval(data);
 	var tableString = '<table border=\"1\">' +
 					'<tr>'+
 					'<th> Spieler(Vor und Nachname)</th>'+
@@ -24,17 +20,18 @@ function tabledraw(array) {
 					'<th>Position</th>'+
 					'<th>Aktiv</th>'+
 					'<th>Rückennummer</th>'+
-					'<th>Geburtsjahr</th>';
+					'<th>Geburtsjahr</th>'+
+					'</tr>';
 	var i;
 	for(i = 0 ; i < arr.length ; i++){
-		tableString += "<td>" + arr[i].firstname + ", " + arr[i].surname + "</td>"
+		tableString += "<tr><td>" + arr[i].firstname + ", " + arr[i].surname + "</td>"
 					+ "<td>" + arr[i].team + "</td>"
 					+ "<td>" + arr[i].headcoach + "</td>"
-					+ "<td>" + arr[i].assisantcoach + "<td>"
+					+ "<td>" + arr[i].asisstantcoach + "</td>"
 					+ "<td>" + arr[i].position + "</td>"
-					+ "<td>" + arr[i].isAktiv + "</td>"
+					+ "<td>" + arr[i].isActive + "</td>"
 					+ "<td>" + arr[i].number + "</td>"
-					+ "<td>" + arr[i].year + "</td>";
+					+ "<td>" + arr[i].year + "</td></tr>";
 	}
 	tableString += "</table>";
 	document.getElementById("table01").innerHTML = tableString;
